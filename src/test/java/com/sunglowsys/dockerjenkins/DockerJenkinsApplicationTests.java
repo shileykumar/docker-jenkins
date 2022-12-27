d8760ec9fbba5a065eada609601adbb8c1e9e25a
shiley <shileykumar@gmail.com>
2022-12-27 23:25:31 +0530

    initial commit

---
+++ src/test/java/com/sunglowsys/dockerjenkins/DockerJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunglowsys.dockerjenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
